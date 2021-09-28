ddb0411d75ea7681877785b43a4638919cc772e7
Eric Murphy <murphye@gmail.com>
2021-09-27 21:31:04 -0700

    Remove HttpFilters, TracingInterceptor

--- src/main/java/org/acme/PetStoreRouter.java
+++ src/main/java/org/acme/PetStoreRouter.java
@@ -12,16 +12,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-//TODO: use x-request-id as Idempotency key https://medium.com/@saurav200892/how-to-achieve-idempotency-in-post-method-d88d7b08fcdd ? 
-// https://datatracker.ietf.org/doc/html/draft-ietf-httpapi-idempotency-key-header-00
-// https://docs.webengage.com/docs/webhooks (Uses x-request-id)
-// https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/observability/tracing
-
-
 @ApplicationScoped
 public class PetStoreRouter {
-
-    static final String SPEC_URL = "https://app.swaggerhub.com/apis/murphye/petstore/1.0.0-oas3";
 
     @Inject
     Vertx vertx;
@@ -30,17 +22,14 @@ public class PetStoreRouter {
     PetStoreService petStoreService;
 
     void init(@Observes Router router) {
-        //router.route().handler(TracingInterceptor.create());
         router.route().handler(BodyHandler.create());
-
-        // Problem with Native https://github.com/vert-x3/vertx-web/issues/2045
-        var routerBuilder = RouterBuilder.createAndAwait(vertx, SPEC_URL);
+        final var routerBuilder = RouterBuilder.createAndAwait(vertx, "https://app.swaggerhub.com/apis/murphye/petstore/1.0.0-oas3");
 
         routerBuilder.operation("listPets").handler(this::listPets);
         routerBuilder.operation("createPets").handler(this::createPets);
         routerBuilder.operation("showPetById").handler(this::showPetById);
 
-        Router apiRouter = routerBuilder.createRouter();
+        final var apiRouter = routerBuilder.createRouter();
 
         apiRouter.errorHandler(404, rc -> {
             JsonObject errorJson = new JsonObject().put("code", 404).put("message",
