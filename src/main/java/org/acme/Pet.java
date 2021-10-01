494e40bea31f1310b257f9dcc3d87b7ed5d9f3f4
Eric Murphy <murphye@gmail.com>
2021-10-01 10:41:06 -0700

    RegisterForReflection

--- src/main/java/org/acme/Pet.java
+++ src/main/java/org/acme/Pet.java
@@ -2,4 +2,7 @@ package org.acme;
 
 import java.util.Optional;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record Pet(Integer id, String name, Optional<String> tag) {}
